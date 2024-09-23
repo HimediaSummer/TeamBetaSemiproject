83cc80f6cf1409a208a798cb1dd0010e7e7a5b06
kimkeny <gustmd9146@gmail.com>
2024-09-23 10:45:30 +0900

    에러 해결(경로문제 해결)

--- Beta/src/main/java/beta/function/account/controller/AccountController.java
+++ Beta/src/main/java/beta/function/account/controller/AccountController.java
@@ -1,4 +1,59 @@
 package beta.function.account.controller;
 
+import beta.function.account.dto.SignupDTO;
+import beta.function.account.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
 public class AccountController {
+
+    private AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/signup")
+    public void signup() {}
+
+    @PostMapping("/signup")
+    public ModelAndView signup(ModelAndView mv,
+                               @ModelAttribute SignupDTO newUserInfo) {
+
+        Integer result = accountService.regist(newUserInfo);
+
+        String message = null;
+
+        if(result == null) {
+            message = "이미 해당 정보로 가입된 회원이 존재합니다.";
+            System.out.println(message);
+
+            mv.setViewName("user/signup");
+        }else if(result == 0) {
+            message = "회원가입에 실패했습니다. 다시 시도해주세요";
+            System.out.println(message);
+
+            mv.setViewName("user/signup");
+        } else if (result >= 1) {
+            message = "회원가입에 성공적으로 완료되었습니다.";
+            System.out.println(message);
+
+            mv.setViewName("auth/login");
+        }else {
+            message = "알 수 없는 오류가 발생했습니다. 다시 시도해보시거나 관리자에게 문의해주세요.";
+            System.out.println(message);
+
+            mv.setViewName("user/signup");
+        }
+
+        return mv;
+    }
 }
