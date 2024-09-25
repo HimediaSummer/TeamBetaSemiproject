bf53aaae3f2fbb6d5c13ac74e6cf8cb4c610b934
soyuul <syuul3139@gmail.com>
2024-09-25 14:47:22 +0900

    오더 기능으로 브랜치 변경

--- Beta/src/main/java/beta/function/game/dao/GameMapper.java
+++ Beta/src/main/java/beta/function/game/dao/GameMapper.java
@@ -5,8 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-// MyBatis의 Mapper 인터페이스임을 나타내는 org.apache.ibatis.annotation 소소의 어노테이션.
-// 이 어노테이션이 정의된 인터페이스는 MyBatis와 연동되어 SQL 매핑을 처리함.
 @Mapper
 public interface GameMapper {
 
