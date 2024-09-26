17d63a4f315418ac6a29c70e33ac4999db7edf30
summerchoco <shkkk90@daum.net>
2024-09-26 17:07:12 +0900

    중간 push 비밀번호찾

--- Beta/src/main/java/beta/function/auth/controller/AuthController.java
+++ Beta/src/main/java/beta/function/auth/controller/AuthController.java
@@ -5,6 +5,11 @@ import beta.function.account.dto.AccountDTO;
 import beta.function.account.dto.SignupDTO;
 import beta.function.account.service.AccountService;
 import beta.function.auth.service.AuthService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +17,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/auth")
@@ -60,7 +67,7 @@ public class AuthController {
     public void findlostpwd(){}
 
     @PostMapping("/lostpwd")
-    public String findlostpwd(String email, Model model) {
+    public String findlostpwd(HttpServletRequest req, HttpServletResponse resp, String email, Model model) throws IOException {
 
         System.out.println("wherepwd 어스 컨트롤러 1 = " );
         AccountDTO wherepwd = authService.lostid(email);
@@ -68,10 +75,19 @@ public class AuthController {
         System.out.println("wherepwd 어스 컨트롤러 2 = " + wherepwd);
         model.addAttribute("wheerepwd",wherepwd);
         System.out.println("wherepwd 어스 컨트롤러 3 = " + wherepwd);
+
+        System.out.println("/session 서블릿 호출됨...");
+
+        // 다시 생각해보니 세션에서 정보를 가져올수가없음 로그인을 안하니까
+        // spring security를 거치지않아서 로그인정보 < 를 요청못함
+        // 그냥 버튼 하나로만들고 화면만 좀 꾸며주고 main에다 합치는걸로
+
 //
 //        if( wherepwd != null ){
 //            authService.changepwd(wherepwd);
 //        }
+
+        resp.sendRedirect("changepwd");
 
         return "auth/lostpwd";
     }
@@ -89,13 +105,6 @@ public class AuthController {
 
         rAttr.addFlashAttribute("successMessage", "성공적으로 수정되었습니다.");
         System.out.println("changepwd 어스 컨트롤러 3  " );
-
-        System.out.println("테스트 로그");
-        System.out.println("테스트 로그2");
-        System.out.println("테스트 로그3");
-        System.out.println("테스트 로그4");
-        System.out.println("테스트 로그5");
-        System.out.println("테스트 로그6");
 
         return "auth/changepwd";
 //        return "redirect:/auth/login";
