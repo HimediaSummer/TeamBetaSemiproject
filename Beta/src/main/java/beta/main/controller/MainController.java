82914a68c1fad244ddba2da017e3a2fd49a188a4
ManaLi <yanhongli86@gmail.com>
2024-10-01 11:37:58 +0900

    CSS 작업 10월 1일 11:40 까지

--- Beta/src/main/java/beta/main/controller/MainController.java
+++ Beta/src/main/java/beta/main/controller/MainController.java
@@ -1,16 +1,40 @@
 package beta.main.controller;
 
+import beta.function.game.dto.GameDTO;
+import beta.function.game.service.GameService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    private final GameService gameService;
+
+    public MainController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
     @GetMapping(value = {"/", "/main"})
-    public ModelAndView main(ModelAndView mv) {
+    public ModelAndView findGameList(ModelAndView mv) {
+
+        List<GameDTO> gameList = gameService.findAllGame();
+        mv.addObject("gameList", gameList);
         mv.setViewName("main/main");
+
         return mv;
+
+//        public String findGameList(Model model) {
+//            List<GameDTO> gameList = gameService.findAllGame();
+//
+//            model.addAttribute("gameList", gameList);
+//
+//            return "game/listAll";
+//        }
+
     }
 
     /* 설명. 유저 권한 설정 체크 */
